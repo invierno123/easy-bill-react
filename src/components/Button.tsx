c82b484dd415bc85386b45a08111938ba09b3c77
invierno <1354002980@qq.com>
2021-03-08 01:07:13 +0800

    测试

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+const Button=styled.button`
+color: antiquewhite;
+  border: none;
+  background: black;
+  &:hover{
+    background: cadetblue;
+  }
+`;
+export default Button;
