696a8afc1203a6d60a49564c9861791430ae2072
invierno <1354002980@qq.com>
2021-03-08 01:13:31 +0800

    删掉无用的文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Button from './components/Button';
-import Box from './components/Box';
+
 function App() {
   return (
    <div>
-     <Box>
-       <Button>hey</Button>
-     </Box>
+
 
    </div>
   );
