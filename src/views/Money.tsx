b0d9ace2d6bef257f7aaa7b9736e8913bb8de7c0
invierno <1354002980@qq.com>
2021-03-26 13:41:32 +0800

    完成 Money 的 样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 const TagsSection = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
 
   > ol {
     margin: 0 -12px;
@@ -32,15 +37,17 @@ const NotesSection = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  
-  >label{
+
+  > label {
     display: flex;
     align-items: center;
-    >span{
+
+    > span {
       margin-right: 16px;
       white-space: nowrap;
     }
-    >input{
+
+    > input {
       display: block;
       width: 100%;
       height: 72px;
@@ -51,83 +58,100 @@ const NotesSection = styled.section`
 `;
 const CategorySection = styled.section`
   font-size: 24px;
->ul{
-  display: flex;
-  background: #c4c4c4;
-  >li{
-    width: 50%;
-    text-align: center;
-    padding: 16px 0;
-    position: relative;
-    &.selected::after{
-      content: '';
-      display: block;
-      height: 3px;
-      background: #333;
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      left: 0;
+
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+      }
     }
   }
-}
 `;
 const NumberPadSection = styled.section`
-display: flex;
+  display: flex;
   flex-direction: column;
-  >output{
+
+  > output {
     background: white;
     font-size: 36px;
     line-height: 72px;
     text-align: right;
     padding: 0 16px;
-    box-shadow: inset  0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset  0  5px 5px -5px rgba(0,0,0,0.25);
-}
-  >.pad{
-    
-    >button{
+    box-shadow: inset  0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+
+  > .pad {
+
+    > button {
       font-size: 18px;
       float: left;
       width: 25%;
       height: 64px;
       border: none;
-      &.ok{
+
+      &.ok {
         height: 128px;
         float: right;
       }
-      &.zero{
+
+      &.zero {
         width: 50%;
       }
-      &:nth-child(1){
+
+      &:nth-child(1) {
         background: #f2f2f2;
       }
-      &:nth-child(2),&:nth-child(5){
+
+      &:nth-child(2), &:nth-child(5) {
         background: #E0E0E0;
       }
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){
-        background:#D3D3D3 ;
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+        background: #D3D3D3;
       }
-      &:nth-child(4),&:nth-child(7),&:nth-child(10){
-        background:#C1C1C1 ;
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+        background: #C1C1C1;
       }
-      &:nth-child(8),&:nth-child(11),&:nth-child(13){
-        background:#BBBBBB ;
+
+      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+        background: #BBBBBB;
       }
-      &:nth-child(12){
+
+      &:nth-child(12) {
         background: #9A9A9A;
       }
-      &:nth-child(14){
-      background:#A9A9A9 ;
+
+      &:nth-child(14) {
+        background: #A9A9A9;
       }
     }
   }
 `;
-
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`;
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -168,7 +192,7 @@ function Money() {
           <button>.</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 
