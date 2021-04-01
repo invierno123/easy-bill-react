0c020fb8a37171e0642d8919ed68375f3a9e70c5
invierno <1354002980@qq.com>
2021-04-01 18:54:44 +0800

    完成 tag 的编辑功能

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -25,9 +25,9 @@ background: white;
   margin-top: 8px;
 `
 const Tag: React.FC = () => {
-  const {findTag} = useTags();
-  let {id} = useParams<Params>();
-  const tag = findTag(parseInt(id));
+  const {findTag,updateTag} = useTags();
+  let {id:idString} = useParams<Params>();
+  const tag = findTag(parseInt(idString));
   return (
     <Layout>
       <TopBar>
@@ -36,7 +36,14 @@ const Tag: React.FC = () => {
         <Icon/>
       </TopBar>
       <InputWrapper>
-          <Input label='标签名' type="text" placeholder='标签名' value={tag.name}/>
+          <Input label='标签名'
+                 type="text"
+                 placeholder='标签名'
+                 value={tag.name}
+          onChange={(e)=> {
+            updateTag(tag.id, {name: e.target.value})
+          }}
+          />
       </InputWrapper>
       <Center>
         <Button>删除标签</Button>
