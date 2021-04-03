4824fa1d20cc51ef64998e2f3a6a433e537d0569
invierno <1354002980@qq.com>
2021-04-03 16:16:40 +0800

    调整滚动条

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -35,7 +35,7 @@ function Statistics() {
   const {getName} = useTags();
   const selectedRecords = records.filter(r => r.category === category);
   const hash: { [K: string]: RecordItem[] } = {};
-  selectedRecords.map(r => {
+  selectedRecords.forEach(r => {
     const key = day(r.createdAt).format('YYYY年MM月DD日');
     if (!(key in hash)) {
       hash[key] = [];
